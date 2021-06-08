55af5ecb63166fc6185f871b855db45dc027f368
Dmitriy Fomin <79067773397@yandex.ru>
2021-06-09 01:41:04 +0300

    lab 5

--- src/ru/fomin/graph/Graph.java
+++ src/ru/fomin/graph/Graph.java
@@ -1,15 +1,86 @@
 package ru.fomin.graph;
 
-/**Граф.*/
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Неориентрированный взвешанный граф.
+ */
 public class Graph {
 
     //матрица смежности
-   private Integer[][] matrix;
+    private Integer[][] matrix;
 
     public Graph(Integer[][] matrix) {
         this.matrix = matrix;
     }
 
+    /**
+     * Возвращает минимальный путь между вершинами графа в виде списка пройденных вершин.
+     * Нумерация вершин начинается с 1.
+     *
+     * @param vertex1 - первая вершина
+     * @param vertex2 - вторая вершина
+     */
+    public List<Integer> getMinWay(Integer vertex1, Integer vertex2) {
+        if (vertex1 < 1 || vertex2 < 1 || vertex1 == vertex2) {
+            throw new IndexOutOfBoundsException("недопустимые значения вершин графа");
+        }
+        return getMinWay(--vertex1, --vertex2, new ArrayList<>());
+    }
 
+    /**
+     * Возвращает минимальный путь между вершинами графа в виде списка пройденных вершин.
+     * Нумерация вершин начинается с 0.
+     *
+     * @param from       - вершина отправления, отсутствует в списке пройденных вершин
+     * @param target     - вершина назначения
+     * @param vertexList - пройденные вершины
+     * @return - null, если отсутствуют непосещённые вершины
+     */
+    private List<Integer> getMinWay(Integer from, Integer target, List<Integer> vertexList) {
+        vertexList.add(from);
+        Integer[] vertexWays = matrix[from];
+        int minWayLength = Integer.MAX_VALUE;
+        List<Integer> newVertexList = null;
+
+        for (int i = 0; i < vertexWays.length; i++) {
+
+            /*Проверяется, что данное ребро существует,
+             * и что данный узел ещё не был посещён.*/
+            if (vertexWays[i] == null || vertexList.contains(i)) {
+                continue;
+            }
+
+            //проверяем, является ли данный узел пунктом назначения
+            if (i == target) {
+                vertexList.add(i);
+                newVertexList = new ArrayList<>(vertexList);
+                break;
+            }
+
+            List<Integer> vertexListOfCurrentVertex = getMinWay(i, target, new ArrayList<>(vertexList));
+            if (vertexListOfCurrentVertex == null) {
+                continue;
+            }
+            int wayLength = getWayLength(vertexListOfCurrentVertex);
+            if (wayLength < minWayLength) {
+                minWayLength = wayLength;
+                newVertexList = vertexListOfCurrentVertex;
+            }
+        }
+        return newVertexList;
+    }
+
+    /**
+     * Возвращает длину пути по списку пройденных узлов.
+     */
+    public int getWayLength(List<Integer> vertexList) {
+        int wayLength = 0;
+        for (int i = 1; i < vertexList.size(); i++) {
+            wayLength += matrix[vertexList.get(i - 1)][vertexList.get(i)];
+        }
+        return wayLength;
+    }
 
 }
--- src/ru/fomin/graph/StarterGraph.java
+++ src/ru/fomin/graph/StarterGraph.java
@@ -1,28 +1,81 @@
 package ru.fomin.graph;
 
+import java.util.List;
+
 public class StarterGraph {
 
     public static void main(String[] args) {
-        Graph graph = new Graph(getAdjacencyMatrix());
-
+        Integer[][] adjacencyMatrix = getAdjacencyMatrix();
+        printMatrix(adjacencyMatrix);
+        Graph graph = new Graph(adjacencyMatrix);
+        List<Integer> vertexesOfMinWay = graph.getMinWay(8, 12);
+        printWay(vertexesOfMinWay);
+        System.out.println("Length of way: " + graph.getWayLength(vertexesOfMinWay));
     }
 
-    private static Integer[][] getAdjacencyMatrix(){
-        return new Integer[][]{
-                {null,5,null,null,null,null,null,8,9,null,null,null,3},
-                {5,null,null,8,null,21,24,16,null,null,null,null,7},
-                {null,null,null,null,2,5,6,22,3,null,5,null,11},
-                {null,8,null,null,9,null,null,null,null,null,null,null,15},
-                {},
-                {},
-                {},
-                {},
-                {},
-                {},
-                {},
-                {},
-                {},
+    /**
+     * Выводит в консоль путь в виде последовательности вершин.
+     */
+    private static void printWay(List<Integer> vertexList) {
+        System.out.println("Full way:");
+        for (int i = 0; i < vertexList.size(); i++) {
+            System.out.print(vertexList.get(i) + 1);
+            if (i != vertexList.size() - 1) {
+                System.out.print(" -> ");
+            }
         }
+        System.out.println();
+    }
+
+    private static Integer[][] getAdjacencyMatrix() {
+        return new Integer[][]{
+                {null, 5, null, null, null, null, null, 8, 9, null, null, null, 3},
+                {5, null, null, 8, null, 21, 24, 16, null, null, null, null, 7},
+                {null, null, null, null, 2, 5, 6, 22, 3, null, 5, null, 11},
+                {null, 8, null, null, 9, null, null, null, null, null, null, null, 15},
+                {null, null, 2, 9, null, null, null, null, null, null, 4, null, 19},
+                {null, 21, 5, null, null, null, null, null, 5, null, null, 6, null},
+                {null, 24, 6, null, null, null, null, null, 13, 3, null, null, null},
+                {8, 16, 22, null, null, null, null, null, 8, null, null, null, null},
+                {9, null, 3, null, null, 5, 13, 8, null, null, 9, null, null},
+                {null, null, null, null, null, null, 3, null, null, null, null, null, null},
+                {null, null, 5, null, 4, null, null, null, 9, null, null, null, null},
+                {null, null, null, null, null, 6, null, null, null, null, null, null, null},
+                {3, 7, 11, 15, 19, null, null, null, null, null, null, null, null},
+        };
+    }
+
+    /**
+     * Выводит в консоль матрицу смежности
+     */
+    private static void printMatrix(Integer[][] matrix) {
+        System.out.print("     ");
+        for (int i = 1; i <= matrix.length; i++) {
+            System.out.print(i + "    ");
+            if (i < 10) {
+                System.out.print(" ");
+            }
+        }
+        System.out.println();
+        for (int i = 0; i < matrix.length; i++) {
+            System.out.print((i + 1) + "   ");
+            if (i < 9) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < matrix.length; j++) {
+                Integer currentValue = matrix[i][j];
+                System.out.print(currentValue);
+                if (currentValue == null) {
+                    System.out.print("  ");
+                } else if (currentValue < 10) {
+                    System.out.print("     ");
+                } else {
+                    System.out.print("    ");
+                }
+            }
+            System.out.println();
+        }
+        System.out.println();
     }
 
 }
